7f7d615825440a7e22004b0838eb0f463afc34bc
Twisneski <tina.wisneski525@gmail.com>
2016-03-07 14:35:29 -0600

    last in to end of list

--- list.js
+++ list.js
@@ -20,9 +20,6 @@ class List {
         current = current.next
       }
       current.next = node
-
-      console.log('more than one', current)
-
     } else {
       this.head = node
     }
@@ -33,4 +30,7 @@ const list = new List
 list.add(new Node('A'))
 list.add(new Node('B'))
 list.add(new Node('C'))
+list.add(new Node('D'))
+list.add(new Node('E'))
+list.add(new Node('F'))
 console.log(list)

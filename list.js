22bdd2e74f7740afde1faef7d0d23b1b4ef1439b
Twisneski <tina.wisneski525@gmail.com>
2016-03-07 14:09:33 -0600

    last in is first in list

---
+++ list.js
@@ -0,0 +1,34 @@
+'use strict'
+
+class Node {
+  constructor (data) {
+    this.payload = data
+    this.next = null
+  }
+}
+
+class List {
+  constructor () {
+    this.head = null
+  }
+
+  add (node) {
+    node.next = this.head
+    this.head = node
+  }
+}
+
+const list = new List
+list.add(new Node('A'))
+list.add(new Node('B'))
+list.add(new Node('C'))
+list.add(new Node('D'))
+list.add(new Node('E'))
+list.add(new Node('F'))
+console.log(list)
+
+
+
+
+
+
